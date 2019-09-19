217bc24ecdb6fe03752dac9dd855df5bc1d31562
Vitor <comercial@sisistemasintegrados.com.br>
2019-09-19 19:26:10 -0300

    Add sharing component, add helvetica neue font

--- src/components/share-form/index.jsx
+++ src/components/share-form/index.jsx
@@ -2,7 +2,43 @@ import React from "react";
 import "./styles.css";
 
 const ShareForm = () => {
-  return <article>share form</article>;
+  return (
+    <article className="container-fluid text-center share-form-article">
+      <h1>Compartilhe a novidade</h1>
+      <h3 className="subtitle">
+        Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+        agora!
+      </h3>
+      <form name="message" method="post">
+        <section className="share-form-section container-fluid">
+          <div>
+            <label>Nome do seu amigo:</label>
+            <input
+              type="text"
+              name="name"
+              pattern="[a-z A-Z]{1,99}"
+              title="O nome deve conter somente letras"
+              required
+            />
+          </div>
+
+          <div>
+            <label>E-mail:</label>
+            <input
+              type="email"
+              name="email"
+              title="Insira o e-mail no formato correto"
+              required
+            />
+          </div>
+        </section>
+        <br />
+        <button type="submit" value="Submit" className="more-products-button">
+          Enviar agora
+        </button>
+      </form>
+    </article>
+  );
 };
 
 export default ShareForm;
